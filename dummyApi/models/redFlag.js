217930b430ef47a8a86369395c9fc01c6b436cac
saladinjake <juwavictor@gmail.com>
2020-10-02 22:49:01 +0100

    chore-#(ch:Created dumy models)

---
+++ dummyApi/models/redFlag.js
@@ -0,0 +1,39 @@
+const incidents = [{
+    id: 1,
+    createdOn: new Date(),
+    createdBy: 1,
+    type: 'red-flag',
+    location: '9.076479, 7.398574',
+    status: 'draft',
+    images: [
+      'https://static.pulse.ng/img/incoming/origs7873831/3206361644-w644-h960/Godwin-Emefiele.jpg',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgFTAylUzrw8NaJQHsojhiC5cYMklzdR43PypR_oUVtt4TNcwEWg.jpg'
+    ],
+    videos: ['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgFTAylUzrw8NaJQHsojhiC5cYMklzdR43PypR_oUVtt4TNcwEWg', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgFTAylUzrw8NaJQHsojhiC5cYMklzdR43PypR_oUVtt4TNcwEWg'],
+    comment: 'Employment scandals in Central Bank of Nigeria (CBN) and Federal Inland Revenue Service (FIRS)'
+  },
+  {
+    id: 2,
+    createdOn: new Date(),
+    createdBy: 2,
+    type: 'red-flag',
+    location: '6.524379, 3.379206',
+    status: 'under investigation',
+    images: ['https://static.pulse.ng/img/incoming/origs7532087/2036362149-w644-h960/babachir-lawal.jpg', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVtbtjMiVvbOLVc7dA53s3_st7BjF-wtTxNu8Tq_-5al0IZBId.jpg'],
+    videos: ['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVtbtjMiVvbOLVc7dA53s3_st7BjF-wtTxNu8Tq_-5al0IZBId', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVtbtjMiVvbOLVc7dA53s3_st7BjF-wtTxNu8Tq_-5al0IZBId'],
+    comment: 'Grass Cutting” scandal of ex-secretary to the Federal Government'
+  },
+  {
+    id: 3,
+    createdOn: new Date(),
+    createdBy: 3,
+    type: 'red-flag',
+    location: '6.444550, 7.490180',
+    status: 'resolved',
+    images: ['https://static.pulse.ng/img/incoming/origs7872357/5196368231-w644-h960/DSuR9f-XUAY9MDF.jpg', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPqMnK2DKy63bhx_-TCivEXE007u6IKzgxcg6kPEw2rohPkbQMIQ.jpg'],
+    videos: ['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVtbtjMiVvbOLVc7dA53s3_st7BjF-wtTxNu8Tq_-5al0IZBId', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVtbtjMiVvbOLVc7dA53s3_st7BjF-wtTxNu8Tq_-5al0IZBId'],
+    comment: '$24 billion NNPC contract scam'
+  }
+];
+
+export default incidents;
---
+++ dummyApi/models/users.js
@@ -0,0 +1,36 @@
+const users = [{
+    id: 1,
+    firstname: 'Saladin',
+    lastname: 'JAKE',
+    othernames: 'Coddy',
+    email: 'juwavictor@gmail.com',
+    phoneNumber: '09087654531',
+    username: 'Xcode',
+    registered: new Date(),
+    isAdmin: true
+  },
+  {
+    id: 2,
+    firstname: 'Bonnyface',
+    lastname: 'Mick',
+    othernames: 'assaa',
+    email: 'bucksbonny@gmail.com',
+    phoneNumber: '08045723898',
+    username: 'luchy',
+    registered: new Date(),
+    isAdmin: false
+  },
+  {
+    id: 3,
+    firstname: '',
+    lastname: '',
+    othernames: '',
+    email: '@gmail.com',
+    phoneNumber: '08048890112',
+    username: 'luchy',
+    registered: new Date(),
+    isAdmin: false
+  }
+];
+
+export default users;
