6c880f7013726b03e5ce6498d49da15e323790a9
saladinjake <juwavictor@gmail.com>
2020-10-03 12:52:37 +0100

    chore-#(ch:Set up mock up data for dummy seeds)

---
+++ api/tests/mock_data.js
@@ -0,0 +1,263 @@
+export const users = [{
+    firstname: '',
+    lastname: 'Rema',
+    othernames: 'Bemas',
+    username: 'Bemas',
+    email: 'Bemas@gmail.com',
+    password: 'password',
+    phoneNumber: '09320302323'
+  }, {
+    firstname: 'Donald',
+    lastname: '',
+    othernames: 'Donald',
+    username: 'Donald33',
+    email: 'donald@yahoo.com',
+    password: 'password',
+    phoneNumber: '09320302323'
+  }, {
+    firstname: 'Dunny',
+    lastname: 'merandra',
+    othernames: '',
+    username: 'Meranda',
+    email: 'meranda@yahoo.com',
+    password: 'password',
+    phoneNumber: '07033337344'
+  }, {
+    firstname: 'Sammy',
+    lastname: 'Allen',
+    othernames: 'Mellan',
+    username: '',
+    email: 'mellan@yahoo.com',
+    password: 'password',
+    phoneNumber: '07012433453'
+  }, {
+    firstname: 'Sammy',
+    lastname: 'Kody',
+    othernames: 'Jinny',
+    username: 'Fireboy',
+    email: '',
+    password: 'password',
+    phoneNumber: '08068291105'
+  }, {
+    firstname: 'Sammy',
+    lastname: 'Kody',
+    othernames: 'Jinny',
+    username: 'Fireboy',
+    email: 'dassy@yahoo.com',
+    password: '',
+    phoneNumber: '07044444563'
+  },
+  {
+    firstname: 'Sammy',
+    lastname: 'Kody',
+    othernames: 'Jinny',
+    username: 'Fireboy',
+    email: 'dassy@yahoo.com',
+    password: 'password',
+    phoneNumber: ''
+  },
+  {
+    firstname: 'Ai7',
+
+    lastname: 'Kody',
+    othernames: 'Jinny',
+    username: 'Fireboy',
+    email: 'dassy@yahoo.com',
+    password: 'password',
+    phoneNumber: '07056734563'
+  },
+  {
+    firstname: 'Sammy',
+    lastname: 'A1',
+    othernames: 'Jinny',
+    username: 'Fireboy',
+    email: 'dassy@yahoo.com',
+    password: 'password',
+    phoneNumber: '07056734563'
+  },
+  {
+    firstname: 'Sammy',
+    lastname: 'Kody',
+    othernames: 'Du',
+    username: 'Fireboy',
+    email: 'dassy@yahoo.com',
+    password: 'password',
+    phoneNumber: '07056734563'
+  },
+  {
+    firstname: 'Sammy',
+    lastname: 'Kody',
+    othernames: 'Jinny',
+    username: 'F',
+    email: 'dassy@yahoo.com',
+    password: 'password',
+    phoneNumber: '07056734563'
+  },
+  {
+    firstname: 'Sammy',
+    lastname: 'Kody',
+    othernames: 'Jinny',
+    username: 'Fireboy',
+    email: 'dassy@',
+    password: 'password',
+    phoneNumber: '07088888888'
+  },
+  {
+    firstname: 'Sammy',
+    lastname: 'Kody',
+    othernames: 'Jinny',
+    username: 'Fireboy',
+    email: 'dassy@yahoo.com',
+    password: 'pass',
+    phoneNumber: '07088888888'
+  },
+  {
+    firstname: 'Sammy',
+    lastname: 'Kody',
+    othernames: 'Jinny',
+    username: 'Fireboy',
+    email: 'dassy@yahoo.com',
+    password: 'password',
+    phoneNumber: '070137'
+  },
+  {
+    firstname: 'Sammy',
+    lastname: 'Kody',
+    othernames: 'Jinny',
+    username: 'Fireboy',
+    email: 'dassy@yahoo.com',
+    password: 'password',
+    phoneNumber: '08068291106'
+  },
+  {
+    firstname: 'Sammy',
+    lastname: 'Kody',
+    othernames: 'Jinny',
+    username: 'Fireboy',
+    email: 'dass32y@yahoo.com',
+    password: 'password',
+    phoneNumber: '08068291106'
+  }
+];
+
+
+export const records = [{
+    location: '6.524379, 3.379206',
+    images: [
+      'https://static.pulse.ng/img/incoming/origs7532087/2036362149-w644-h960/babachir-lawal.jpg',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVtbtjMiVvbOLVc7dA53s3_st7BjF-wtTxNu8Tq_-5al0IZBId.jpg'
+    ],
+    videos: [
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVtbtjMiVvbOLVc7dA53s3_st7BjF-wtTxNu8Tq_-5al0IZBId',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVtbtjMiVvbOLVc7dA53s3_st7BjF-wtTxNu8Tq_-5al0IZBId'
+    ],
+    comment: 'Grass Cutting” scandal of ex-secretary to the Federal Government'
+  },
+  {
+    location: '',
+    images: [
+      'https://static.pulse.ng/img/incoming/origs7532087/2036362149-w644-h960/babachir-lawal.jpg',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVtbtjMiVvbOLVc7dA53s3_st7BjF-wtTxNu8Tq_-5al0IZBId.jpg'
+    ],
+    videos: [
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVtbtjMiVvbOLVc7dA53s3_st7BjF-wtTxNu8Tq_-5al0IZBId',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVtbtjMiVvbOLVc7dA53s3_st7BjF-wtTxNu8Tq_-5al0IZBId'
+    ],
+    comment: 'Grass Cutting” scandal of ex-secretary to the Federal Government'
+  },
+  {
+    location: '6.524379, 3.379206',
+    images: '',
+    videos: [
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVtbtjMiVvbOLVc7dA53s3_st7BjF-wtTxNu8Tq_-5al0IZBId',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVtbtjMiVvbOLVc7dA53s3_st7BjF-wtTxNu8Tq_-5al0IZBId'
+    ],
+    comment: 'Grass Cutting” scandal of ex-secretary to the Federal Government'
+  },
+  {
+    location: '6.524379, 3.379206',
+    images: [
+      'https://static.pulse.ng/img/incoming/origs7532087/2036362149-w644-h960/babachir-lawal.jpg',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVtbtjMiVvbOLVc7dA53s3_st7BjF-wtTxNu8Tq_-5al0IZBId.jpg'
+    ],
+    videos: '',
+    comment: 'Grass Cutting” scandal of ex-secretary to the Federal Government'
+  },
+  {
+    location: '6.524379, 3.379206',
+    images: [
+      'https://static.pulse.ng/img/incoming/origs7532087/2036362149-w644-h960/babachir-lawal.jpg',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVtbtjMiVvbOLVc7dA53s3_st7BjF-wtTxNu8Tq_-5al0IZBId.jpg'
+    ],
+    videos: [
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVtbtjMiVvbOLVc7dA53s3_st7BjF-wtTxNu8Tq_-5al0IZBId',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVtbtjMiVvbOLVc7dA53s3_st7BjF-wtTxNu8Tq_-5al0IZBId'
+    ],
+    comment: ''
+  },
+  {
+    location: '6.524379',
+    images: [
+      'https://static.pulse.ng/img/incoming/origs7532087/2036362149-w644-h960/babachir-lawal.jpg',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVtbtjMiVvbOLVc7dA53s3_st7BjF-wtTxNu8Tq_-5al0IZBId.jpg'
+    ],
+    videos: [
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVtbtjMiVvbOLVc7dA53s3_st7BjF-wtTxNu8Tq_-5al0IZBId',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVtbtjMiVvbOLVc7dA53s3_st7BjF-wtTxNu8Tq_-5al0IZBId'
+    ],
+    comment: 'Grass Cutting” scandal of ex-secretary to the Federal Government'
+  },
+  {
+    location: '6.524379, 3.379206',
+    images: [56],
+    videos: [
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVtbtjMiVvbOLVc7dA53s3_st7BjF-wtTxNu8Tq_-5al0IZBId',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVtbtjMiVvbOLVc7dA53s3_st7BjF-wtTxNu8Tq_-5al0IZBId'
+    ],
+    comment: 'Grass Cutting” scandal of ex-secretary to the Federal Government'
+  },
+  {
+    location: '6.524379, 3.379206',
+    images: [
+      'https://static.pulse.ng/img/incoming/origs7532087/2036362149-w644-h960/babachir-lawal.jpg',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVtbtjMiVvbOLVc7dA53s3_st7BjF-wtTxNu8Tq_-5al0IZBId.jpg'
+    ],
+    videos: [90],
+    comment: 'Grass Cutting” scandal of ex-secretary to the Federal Government'
+  },
+  {
+    location: '6.524379, 3.379206',
+    images: [
+      'https://static.pulse.ng/img/incoming/origs7532087/2036362149-w644-h960/babachir-lawal.jpg',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVtbtjMiVvbOLVc7dA53s3_st7BjF-wtTxNu8Tq_-5al0IZBId.jpg'
+    ],
+    videos: [
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVtbtjMiVvbOLVc7dA53s3_st7BjF-wtTxNu8Tq_-5al0IZBId',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVtbtjMiVvbOLVc7dA53s3_st7BjF-wtTxNu8Tq_-5al0IZBId'
+    ],
+    comment: 1234
+  },
+  {
+    location: '6.524379, 3.379206',
+    images: {},
+    videos: [
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVtbtjMiVvbOLVc7dA53s3_st7BjF-wtTxNu8Tq_-5al0IZBId',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVtbtjMiVvbOLVc7dA53s3_st7BjF-wtTxNu8Tq_-5al0IZBId'
+    ],
+    comment: 1234
+  },
+  {
+    location: '6.524379, 3.379206',
+    images: [
+      'https://static.pulse.ng/img/incoming/origs7532087/2036362149-w644-h960/babachir-lawal.jpg',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVtbtjMiVvbOLVc7dA53s3_st7BjF-wtTxNu8Tq_-5al0IZBId.jpg'
+    ],
+    videos: {},
+    comment: 1234
+  }
+];
+
+export default {
+  users,
+  records
+};
